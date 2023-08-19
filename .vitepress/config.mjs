101d502242dafee0c1360e18f8539c684b561466
Jinhyuk Lee <eces92@gmail.com>
2023-08-20 00:34:54 +0900

    Clean url

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "selectfromuser.com",
   description: "Select Admin",
+  cleanUrls: true,
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
