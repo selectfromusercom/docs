fc8c5b40832d5654dbb537f10e0818c18a4b6f14
Jinhyuk Lee <eces92@gmail.com>
2023-09-09 17:34:37 +0900

    guide, changelog: 가이드, 변경내역 추가

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -22,41 +22,138 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Reference', link: '/start' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'Reference', link: '/layout' },
+      { text: 'Changelog', link: '/changelog/' },
       { text: 'Community', link: 'https://ask.selectfromuser.com' },
     ],
 
-    sidebar: [
-      {
-        text: 'Overview',
-        items: [
-          { text: '시작하기', link: '/start' },
-          { text: '개념 및 설계 구조', link: '/introduction' },
-        ]
-      },
-      {
-        text: 'Reference',
-        items: [
-          { text: 'Layout', link: '/layout' },
-          { text: 'Menus', link: '/menus' },
-          { text: 'Pages', link: '/pages' },
-          { text: 'Blocks', link: '/blocks' },
-          { text: 'Columns', link: '/columns' },
-          { text: 'Params', link: '/params' },
-          { text: 'Modals', link: '/modals' },
-          { text: 'Actions', link: '/actions' },
-        ]
-      },
-      {
-        text: 'Links',
-        items: [
-          { text: '튜토리얼 및 가이드', link: 'https://docs.selectfromuser.com/docs' },
-          { text: 'Examples', link: 'https://showroom.selectfromuser.com' },
-          { text: 'Changelog', link: 'https://docs.selectfromuser.com/changelog' },          
-          { text: '로그인', link: 'https://app.selectfromuser.com' },
-        ]
-      },
-    ],
+    sidebar: {
+      '/guide/': [
+        // {
+        //   items: [
+        //   ]
+        // },
+        {
+          text: '어드민 만들기',
+          items: [
+            { text: '시작하기', link: '/guide/'},
+            { 
+              text: '어드민 편집', 
+              link: '/guide/admin-edit', 
+              collapsed: true,
+              items: [
+                { text: '편집 결과 미리보기', link: '/guide/admin-edit-preview'},
+              ],
+            },
+            { 
+              text: '메뉴 구성', 
+              // link: '/guide/aa', 
+              collapsed: true,
+              items: [
+                { text: '메뉴 권한 설정'},
+                { text: '메뉴 path 이름 규칙'},
+                { text: '계층 메뉴 만들기'},
+              ],
+            },
+            { 
+              text: '데이터 조회 페이지', 
+              // link: '/guide/aa', 
+              collapsed: true,
+              items: [
+                { text: '데이터 조회 페이지' },
+                { text: '테이블 페이지네이션' },
+                { text: '테이블 정렬 방법' },
+                { text: '테이블 필드 이름 바꾸기' },
+                { text: '테이블 간편 검색' },
+                { text: '테이블 검색 조건 추가' },
+                { text: '테이블 필드 포맷 설정' },
+                { text: '차트, 그래프로 표시' },
+                { text: '페이지 이동' },
+                { text: '칸반 보드로 보기' },
+              ],
+            },
+          ]
+        },
+        {
+          text: '샘플',
+          items: [
+            
+          ]
+        },
+        {
+          text: 'SQL 팁과 유의사항',
+          items: [
+            
+          ]
+        },
+        {
+          text: '리소스 연결',
+          items: [
+            
+          ]
+        },
+        {
+          text: '팀 관리하기',
+          items: [
+            
+          ]
+        },
+        {
+          text: '로그 및 보안',
+          items: [
+            
+          ]
+        },
+        {
+          text: '컴포넌트',
+          items: [
+            
+          ]
+        },
+      ],
+      '/changelog': [
+        {
+          items: [
+            { text: 'CHANGELOG.md', link: '/changelog/'},
+            { text: '2023-08-03', link: '/changelog/2023-08-03' },
+            { text: '2023-07-07', link: '/changelog/2023-07-07' },
+            { text: '2023-06-08', link: '/changelog/2023-06-08' },
+          ]
+        },
+      ],
+      '/': [
+        {
+          text: 'Overview',
+          items: [
+            // { text: '시작하기', link: '/start' },
+            { text: '개념 및 설계 구조', link: '/introduction' },
+          ]
+        },
+        {
+          text: 'Reference',
+          items: [
+            { text: 'Layout', link: '/layout' },
+            { text: 'Menus', link: '/menus' },
+            { text: 'Pages', link: '/pages' },
+            { text: 'Blocks', link: '/blocks' },
+            { text: 'Columns', link: '/columns' },
+            { text: 'Params', link: '/params' },
+            { text: 'Modals', link: '/modals' },
+            { text: 'Actions', link: '/actions' },
+          ]
+        },
+        {
+          text: 'Links',
+          items: [
+            { text: '샘플 예제', link: 'https://showroom.selectfromuser.com' },
+            { text: '로그인', link: 'https://app.selectfromuser.com' },
+          ]
+        },
+      ]
+    },
+    // sidebar: [
+    // ],
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/selectfromusercom/docs' }
---
+++ changelog/index.md
@@ -0,0 +1,31 @@
+---
+# outline: deep
+---
+
+# 변경내역
+
+## 2023-09-01
+
+### Add
+
+- params format: address
+- block display: calendar
+- actions showDownload csv, openUrl
+- columns format: image
+- columns format: table
+- params validateFn
+- block responseErrorFn
+- block requestSubmitFn
+- MongoDB block queryFn
+
+### Fix
+
+- VPC Peering Connection 가용영역 분리
+- autoload 개선
+- tabOptions 개선
+- display col-1, col-2, col-3, col-4 개선
+- tdClass, tdClass 개선
+- 어드민 첫 접속시 커넥션 풀 안정성 개선
+
+
+https://blog.selectfromuser.com/newsletter-2023-08/
