fc3053bf8fa2b3e044e6eeb66d78f06c70bccb81
hk <99627293+hkm33@users.noreply.github.com>
2025-08-20 04:06:09 +0000

    update nav

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -41,7 +41,7 @@ export default defineConfig({
     nav: [
       { text: 'Guide', link: '/guide/' },
       { text: 'Reference', link: '/reference' },
-      { text: 'Changelog', link: 'https://github.com/eces/select/blob/main/CHANGELOG.md' },
+      // { text: 'Changelog', link: 'https://github.com/eces/select/blob/main/CHANGELOG.md' },
       { text: 'Community', link: 'https://ask.selectfromuser.com' },
     ],
 
